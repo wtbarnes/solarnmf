b1ac4fd4fbc3199015439371b0987fca39fc2762
Will Barnes <wtb2@rice.edu>
2015-04-01 19:19:56 -0500

    Fixes for HALS algorithm

--- solarnmf_main.py
+++ solarnmf_main.py
@@ -24,10 +24,10 @@ Q = 3
 results = snf.make_t_matrix("simulation",nx=NX,ny=NY,p=P,format='matrix')
 
 #Initialize the U and V matrices
-u_initial,v_initial,A_initial = snf.initialize_uva(NX,NY,Q,10,10,results['T'])
+u_initial,v_initial,A_initial = snf.initialize_uva(NX,NY,Q,10,50,results['T'])
 
 #Start the minimizer
-u,v,A,div = snf.minimize_div(uinitial,vinitial,results['T'],ainitial,500,1.0e-5)
+u,v,A,div = snf.minimize_div(u_initial,v_initial,results['T'],A_initial,500,1.0e-5)
 
 #Plot the total results for the observation and the prediction
 spr.plot_obsVpred(results['T'],A)
