f73a4207b263d0cf49e94a9b5f585b4fe075de00
Will Barnes <wtb2@rice.edu>
2015-04-02 18:20:02 -0500

    Adding class that will contain all routines needed to perform NMF for selected divergence measure and update rules

---
+++ solarnmf_learn.py
@@ -0,0 +1,12 @@
+#solarnmf_learn.py
+
+#Will Barnes
+#2 April 2015
+
+#Import needed modules
+import numpy as np
+
+class SeparateSources(object):
+    """Class that performs BSS using specified method for given observation matrix"""
+    
+    def __init__()
