d9e1019e5ab8610fc696081e5047913b8def5c73
Will Barnes <wtb2@rice.edu>
2015-04-16 16:00:21 -0500

    Fixes for plotting results from data rather than simulated gaussian sources

--- solarnmf_main.py
+++ solarnmf_main.py
@@ -21,12 +21,12 @@ from scipy.ndimage.interpolation import rotate
 
 nx = 500
 ny = 500
-p = 10
+p = 15
 q = p
 angle = 45.0
-fn = '/Users/willbarnes/Desktop/frames(AIA_131).2D_lt.v2m'
+fn = '/home/wtb2/Desktop/ts_335_cut_test.txt'
 
-params = {'eps':1.0e-4,'psi':1.0e-16,'sparse_u':0.125,'sparse_v':0.125,'reg_0':20.0,'reg_tau':50.0,'max_i':1000,'r':10,'r_iter':10}
+params = {'eps':1.0e-4,'psi':1.0e-16,'sparse_u':0.125,'sparse_v':0.125,'reg_0':20.0,'reg_tau':50.0,'max_i':100,'r':5,'r_iter':10}
 params['lambda_1'] = 0.0001
 params['lambda_2'] = 0.0001
 params['alpha'] = 0.8
@@ -34,16 +34,15 @@ params['l_toeplitz'] = 5
 params['div_measure'] = 'multiplicative_reg_sparse'
 params['update_rules'] = 'chen_cichocki_reg_sparse'
 
-data = MakeData('simulation','timeseries',filename=fn,nx=nx,ny=ny,p=p,angle=angle)
-target,T,Tmat = data.make_t_matrix()
+data = MakeData('data','timeseries',filename=fn,nx=nx,ny=ny,p=p,angle=angle)
+T,Tmat = data.make_t_matrix()
 
 minimizer = SeparateSources(Tmat,q,params)
 u_temp,v_temp,A_temp = minimizer.initialize_uva()
 u,v,A,div = minimizer.minimize_div(u_temp,v_temp,minimizer.max_i)
 
-plotter = MakeBSSPlots('simulation','timeseries',u,v,A,T,div,target=target,nx=nx,ny=ny,angle=angle,Tmat=Tmat)
+ny_r,nx_r = T.shape[0],T.shape[0]
+plotter = MakeBSSPlots('data','timeseries',u,v,A,T,div,angle=angle,Tmat=Tmat,ny=ny_r,nx=nx_r)
 plotter.plot_obs_pred_total()
 plotter.plot_obs_pred_sources()
 plotter.plot_div()
-    
-    
--- solarnmf_observations.py
+++ solarnmf_observations.py
@@ -10,19 +10,19 @@ from scipy.ndimage.interpolation import rotate
 class MakeData(object):
     """Class for constructing data, either simulation or observational, for use in NMF or BSS methods
     """
-    
+
     def __init__(self,toption,input_type,**kwargs):
         """Constructor for MakeData class"""
-        
+
         #Set observation matrix type
         self.toption = toption
         self.input_type = input_type
-        
+
         self.psi = 1.0e-12
         self.ngrid_y = 15
         self.ngrid_x = 15
         self.noise_level = 0.05
-        
+
         #Check timeseries options
         if self.input_type == 'timeseries':
             if 'angle' not in kwargs:
@@ -30,7 +30,7 @@ class MakeData(object):
                 print "angle not specified. Setting angle to ",self.angle
             else:
                 self.angle = kwargs['angle']
-                
+
         #Check simulation and data options
         if self.toption == 'simulation':
             if 'nx' not in kwargs:
@@ -38,24 +38,24 @@ class MakeData(object):
                 print "nx not specified. Setting nx to ",self.nx
             else:
                 self.nx = kwargs['nx']
-            
+
             if 'ny' not in kwargs:
                 self.ny = 50
-                print "ny not specified. Setting ny to ",self.ny        
+                print "ny not specified. Setting ny to ",self.ny
             else:
                 self.ny = kwargs['ny']
-            
+
             if 'p' not in kwargs:
                 self.p = 5
                 print "p not specified. Setting p to ",self.p
             else:
                 self.p = kwargs['p']
-                
+
             self.grid_y = int(self.ny/self.ngrid_y)
             self.grid_x = int(self.nx/self.ngrid_x)
             self.sigma_y = float(self.grid_y/2.0)
             self.sigma_x = float(self.grid_x/2.0)
-            
+
         elif self.toption == 'data':
             if 'filename' not in kwargs:
                 raise ValueError("'filename' needs to be specified in kwargs in order to load data.")
@@ -63,49 +63,51 @@ class MakeData(object):
                 self.filename = kwargs['filename']
         else:
             raise ValueError("Unknown observation option. Use either 'simulation' or 'data'.")
-            
-        
+
+
     def make_t_matrix(self):
         """Construct matrix or timeseries to factorize"""
-        
+
         if self.toption == 'simulation':
             #Call the make gaussians function
             target,T = self.make_gaussians()
             T[np.where(T<self.psi)] = self.psi
-            
+
             if self.input_type == 'matrix':
                 return target,T
-                
+
             else:
                 Tmat = self.ts_to_mat(T)
                 return target,T,Tmat
-                
+
         else:
-            
+
             T = np.loadtxt(self.filename)
             T /= np.max(T)
             T[np.where(T<self.psi)] = self.psi
-            
+
             if self.input_type == 'matrix':
                 return T
-                
+
             elif self.input_type == 'timeseries':
+                self.ny = T.shape[0]
+                self.sigma_y = float(int(self.ny/self.ngrid_y)/2.0)
                 Tmat = self.ts_to_mat(T)
                 return T,Tmat
-                
+
             else:
                 raise ValueError("Unknown input type. Use either 'timeseries' or 'matrix'.")
-                
-            
+
+
     def make_gaussians(self):
         """Construct simulated gaussian matrices or timeseries"""
-            
+
         target = []
         centers = []
-        
+
         #Seed random number generator
         np.random.seed()
-        
+
         if self.input_type == 'timeseries':
             #Initialize total
             T = np.zeros(self.nx)
@@ -113,10 +115,10 @@ class MakeData(object):
             #Create series of Gaussians
             for i in range(self.ngrid_x):
                 centers.append((i+0.5)*self.grid_x)
-                
+
             t = np.linspace(0,self.nx-1,self.nx)
             n_centers = self.ngrid_x
-            
+
             for i in range(self.p):
                 i_center = np.random.randint(0,n_centers-1)
                 #Create the Gaussian
@@ -128,18 +130,18 @@ class MakeData(object):
                 #Update centers list
                 centers.remove(centers[i_center])
                 n_centers += -1
-                
-            
+
+
         elif self.input_type == 'matrix':
             #Calculate standard deviations in x and y
             sigma_x = self.sigma_x*np.ones((1,self.p))
             sigma_y = self.sigma_y*np.ones((1,self.p))
-            
+
             #Generate random center positions for pulses (normalized to [0,1])
             for i in range(self.ngrid_y):
                 for j in range(self.ngrid_x):
                     centers.append(((i + 0.5)*self.grid_y,(j + 0.5)*self.grid_x))
-                    
+
             n_centers  = self.ngrid_y*self.ngrid_x
 
             #Preallocate for T matrix
@@ -160,30 +162,30 @@ class MakeData(object):
                 #Update centers list and length
                 centers.remove(centers[i_center])
                 n_centers += -1
-                
-            
+
+
         else:
             raise ValueError("Unknown input type. Use either 'timeseries' or 'matrix'.")
-        
-        T = self.add_noise(T)    
+
+        T = self.add_noise(T)
         T = T/np.max(T)
-            
+
         return target,T
-        
-        
+
+
     def ts_to_mat(self,x):
         """Format timeseries as matrix for NMF or BSS factorization."""
-            
+
         #x_smooth = self.ts_smooth(x,window_length=11,window='hanning')
-        
+
         x_mat = self.ts_by_gaussian(x)
-        
+
         if self.angle != 0:
             x_mat  = rotate(x_mat,self.angle)
-            
+
         return x_mat
-        
-            
+
+
     def ts_smooth(self,x,**kwargs):
         """Smoothing algorithm from Scipy.org Cookbook. URL: http://wiki.scipy.org/Cookbook/SignalSmooth
 
@@ -209,7 +211,7 @@ class MakeData(object):
         numpy.hanning,numpy.hamming,numpy.bartlett,numpy.blackman,numpy.convolve
 
         """
-        
+
         #Provide some checks on our inputs to make sure they are valid
 
         if x.ndim != 1:
@@ -232,11 +234,11 @@ class MakeData(object):
 
         #Return the smoothed time series
         return np.convolve(w/w.sum(),s,mode='valid')
-        
-    
+
+
     def ts_by_gaussian(self,x):
         """Convert time series to matrix with some spread defined by a gaussian with standard deviation sigma."""
-    
+
         #Set up Gaussian to filter results through
         t = np.linspace(0,self.ny-1,self.ny)
         mu = float(self.ny-1.0)/2.0
@@ -250,8 +252,8 @@ class MakeData(object):
 
         #Return the filtered matrix
         return np.transpose(np.dot(x_mat,np.transpose(xfilt_mat)))
-        
-        
+
+
     def add_noise(self,mat):
         """Add noise to matrix"""
 
@@ -261,8 +263,6 @@ class MakeData(object):
             mat += self.noise_level*np.random.rand(nr,nc)
         else:
             n = mat.shape[0]
-            mat += self.noise_level*np.random.rand(n) 
-            
-        return mat 
-        
-        
+            mat += self.noise_level*np.random.rand(n)
+
+        return mat
