Fixed problems with rotation caused by shift from improper cuts and mismatch between scale of gaussian used in outer product with timeseries; added function in plotting class that will match observation and predicted source

solarnmf_main.py
@@ -16,10 +16,12 @@ from solarnmf_learn import SeparateSources
 from solarnmf_plotting import MakeBSSPlots
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.interpolation import rotate
 
-nx = 1000
-ny = 50
-p = 3
+nx = 500
+ny = 500
+p = 9
 q = p
 angle = 45.0
 fn = '/Users/willbarnes/Desktop/frames(AIA_131).2D_lt.v2m'
@@ -27,8 +29,8 @@ fn = '/Users/willbarnes/Desktop/frames(AIA_131).2D_lt.v2m'
 params = {'eps':1.0e-4,'psi':1.0e-16,'sparse_u':0.125,'sparse_v':0.125,'reg_0':20.0,'reg_tau':50.0,'max_i':1000,'r':10,'r_iter':10}
 params['lambda_1'] = 0.0001
 params['lambda_2'] = 0.0001
-params['alpha'] = 0.5
-params['l_toeplitz'] = 3
+params['alpha'] = 0.8
+params['l_toeplitz'] = 5
 params['div_measure'] = 'multiplicative_reg_sparse'
 params['update_rules'] = 'chen_cichocki_reg_sparse'
 
@@ -39,7 +41,9 @@ minimizer = SeparateSources(Tmat,q,params)
 u_temp,v_temp,A_temp = minimizer.initialize_uva()
 u,v,A,div = minimizer.minimize_div(u_temp,v_temp,minimizer.max_i)
 
-plotter = MakeBSSPlots('simulation','timeseries',u,v,A,T,div,target=target,Tmat=Tmat,angle=-angle)
+plotter = MakeBSSPlots('simulation','timeseries',u,v,A,T,div,target=target,nx=nx,ny=ny,angle=angle,Tmat=Tmat)
 plotter.plot_obs_pred_total()
 plotter.plot_obs_pred_sources()
 plotter.plot_div()
+    
+    

solarnmf_observations.py
@@ -18,7 +18,6 @@ class MakeData(object):
         self.toption = toption
         self.input_type = input_type
         
-        self.sigma = 0.08
         self.psi = 1.0e-12
         self.ngrid_y = 9
         self.ngrid_x = 9
@@ -54,6 +53,8 @@ class MakeData(object):
                 
             self.grid_y = int(self.ny/self.ngrid_y)
             self.grid_x = int(self.nx/self.ngrid_x)
+            self.sigma_y = float(self.grid_y/2.0)
+            self.sigma_x = float(self.grid_x/2.0)
             
         elif self.toption == 'data':
             if 'filename' not in kwargs:
@@ -100,38 +101,41 @@ class MakeData(object):
         """Construct simulated gaussian matrices or timeseries"""
             
         target = []
+        centers = []
         
         #Seed random number generator
         np.random.seed()
         
         if self.input_type == 'timeseries':
             #Initialize total
-            T = 0.0
+            T = np.zeros(self.nx)
             #Simulate 1D timeseries
             #Create series of Gaussians
-            t = np.linspace(-1,1,self.nx)
-            for i in range(self.p):
+            for i in range(self.ngrid_x):
+                centers.append((i+0.5)*self.grid_x)
                 
-                #Get random sign
-                if np.random.rand() > 0.5:
-                    mu_sign = -1.0
-                else:
-                    mu_sign = 1.0
+            t = np.linspace(0,self.nx-1,self.nx)
+            n_centers = self.ngrid_x
+            
+            for i in range(self.p):
+                i_center = np.random.randint(0,n_centers-1)
                 #Create the Gaussian
-                temp = np.random.rand()*np.exp(-(t+mu_sign*np.random.rand())**2/(2.0*self.sigma**2))
+                temp = np.exp(-(t-centers[i_center])**2/(2.0*self.sigma_x**2))
                 #Add to total
                 T = T + temp
                 #Save the components
                 target.append(temp)
+                #Update centers list
+                centers.remove(centers[i_center])
+                n_centers += -1
                 
             
         elif self.input_type == 'matrix':
             #Calculate standard deviations in x and y
-            sigma_x = float(self.grid_x/2.0)*np.ones((1,self.p))
-            sigma_y = float(self.grid_y/2.0)*np.ones((1,self.p))
+            sigma_x = self.sigma_x*np.ones((1,self.p))
+            sigma_y = self.sigma_y*np.ones((1,self.p))
             
             #Generate random center positions for pulses (normalized to [0,1])
-            centers = []
             for i in range(self.ngrid_y):
                 for j in range(self.ngrid_x):
                     centers.append(((i + 0.5)*self.grid_y,(j + 0.5)*self.grid_x))
@@ -172,10 +176,10 @@ class MakeData(object):
             
         #x_smooth = self.ts_smooth(x,window_length=11,window='hanning')
         
-        x_mat = self.ts_by_gaussian(x,self.ny,0.15)
+        x_mat = self.ts_by_gaussian(x)
         
         if self.angle != 0:
-            x_mat  = self.crop_and_rotate(x_mat,self.angle)
+            x_mat  = rotate(x_mat,self.angle)
             
         return x_mat
         
@@ -230,15 +234,16 @@ class MakeData(object):
         return np.convolve(w/w.sum(),s,mode='valid')
         
     
-    def ts_by_gaussian(self,x,dim2,sigma):
+    def ts_by_gaussian(self,x):
         """Convert time series to matrix with some spread defined by a gaussian with standard deviation sigma."""
     
         #Set up Gaussian to filter results through
-        t = np.linspace(0,1,dim2)
-        xfilt = np.exp(-(t - 0.5)**2/(2*sigma**2))
+        t = np.linspace(0,self.ny-1,self.ny)
+        mu = float(self.ny-1.0)/2.0
+        xfilt = np.exp(-(t - mu)**2/(2.0*self.sigma_y**2))
 
         #Mimic matrix multiplication
-        xfilt_mat = np.zeros([dim2,1])
+        xfilt_mat = np.zeros([self.ny,1])
         x_mat = np.zeros([len(x),1])
         xfilt_mat[:,0] = xfilt
         x_mat[:,0] = x
@@ -246,24 +251,6 @@ class MakeData(object):
         #Return the filtered matrix
         return np.transpose(np.dot(x_mat,np.transpose(xfilt_mat)))
         
-        
-    def crop_and_rotate(self,x_mat,angle):
-    
-        #Find the backgound value
-        bg_val = np.min(x_mat[np.where(x_mat>np.max(x_mat)/100.0)])
-    
-        #Rotate the image and interpolate as necessary
-        x_rot = rotate(x_mat,angle)
-    
-        #Find bounds by subtracting out background
-        row_bounds,col_bounds = np.where(x_rot>bg_val)
-        top = np.min(row_bounds)
-        bottom = np.max(row_bounds)
-        left = np.min(col_bounds)
-        right = np.max(col_bounds)
-    
-        #Return trimmed matrix
-        return x_rot[top:bottom,left:right]
         
     def add_noise(self,mat):
         """Add noise to matrix"""

solarnmf_plotting.py
@@ -19,7 +19,6 @@ class MakeBSSPlots(object):
         self.T = T
         self.div = div
         self.q = self.u.shape[1]
-        self.ts_cut = np.unravel_index(self.A.argmax(),self.A.shape)[0]
         
         self.fs = 18
         self.cm = 'hot'
@@ -27,10 +26,16 @@ class MakeBSSPlots(object):
         self.print_dpi = 1000
         self.fig_size = (12,10)
         
+        self.ny = kwargs['ny']
+        self.nx = kwargs['nx']
         if 'angle' not in kwargs:
             self.angle = 0.0
         else:
             self.angle = kwargs['angle']
+        
+        self.A = self.rotate_back(A)
+        self.get_components()
+        self.ts_cut = np.unravel_index(self.A.argmax(),self.A.shape)[0]
         
         if self.toption == 'simulation':
             try:
@@ -43,11 +48,31 @@ class MakeBSSPlots(object):
             except:
                 raise ValueError("Please specify matrix representation of time series when using 1D representation.")
                 
+    def rotate_back(self,mat):
+        """Rotate back and reshape"""
+        #rotate matrix back
+        if self.angle != 0.0:
+            mat_rot = rotate(mat,-self.angle)
+        else:
+            mat_rot = mat
+        #get indices of rotated matrix
+        ny_r,nx_r = mat_rot.shape
+        #calculate differences
+        delta_y = int(np.round((ny_r - self.ny)/2.0)) 
+        delta_x = int(np.round((nx_r - self.nx)/2.0))
+        #Return cut and rotated matrix
+        return mat_rot[delta_y:((ny_r - delta_y)),delta_x:((nx_r - delta_x))]
+    
+    
+    def get_components(self):
+        """Separate A matrix into components"""
         self.components = []
-        self.make_components()
-
-
+        for i in range(self.q):
+            self.components.append(self.rotate_back(np.outer(self.u[:,i],self.v[i,:])))
+        
+    
     def plot_obs_pred_total(self,**kwargs):
+        """Plot original observation and recovered result"""
         if self.input_type == 'matrix':
             fig,ax = plt.subplots(1,2,figsize=self.fig_size)
             fig.tight_layout()
@@ -76,11 +101,16 @@ class MakeBSSPlots(object):
 
 
     def plot_obs_pred_sources(self,**kwargs):
+        """Plot recovered sources against the original components"""
         try:
             rows = max(self.q,len(self.target))
+            pairs = self.match_closest()
         except:
             rows = self.q
-
+            pairs = []
+            [pairs.append((i,i)) for i in range(self.q)]
+            
+            
         if self.input_type == 'matrix':
             fig,ax = plt.subplots(rows,2,figsize=self.fig_size)
             fig.tight_layout()
@@ -88,18 +118,17 @@ class MakeBSSPlots(object):
             ax[0,1].set_title(r'Predictions',fontsize=self.fs)
             for i in range(rows):
                 try:
-                    im = ax[i,0].imshow(self.target[i],cmap=self.cm)
+                    im = ax[i,0].imshow(self.target[pairs[i][0]],cmap=self.cm)
                     ax[i,0].xaxis.set_ticklabels([])
                     ax[i,0].yaxis.set_ticklabels([])
                     fig.colorbar(im,cax=make_axes_locatable(ax[i,0]).append_axes("right","5%",pad="3%"),ticks=[np.min(self.target[i]),(np.max(self.target[i])-np.min(self.target[i]))/2.0,np.max(self.target[i])])
                 except:
                     pass
                 try:
-                    ai = np.outer(self.u[:,i],self.v[i,:])
-                    im = ax[i,1].imshow(ai,cmap=self.cm)
+                    im = ax[i,1].imshow(self.components[pairs[i][1]],cmap=self.cm)
                     ax[i,1].xaxis.set_ticklabels([])
                     ax[i,1].yaxis.set_ticklabels([])
-                    fig.colorbar(im,cax=make_axes_locatable(ax[i,1]).append_axes("right","5%",pad="3%"),ticks=[np.min(ai),(np.max(ai)-np.min(ai))/2.0,np.max(ai)])
+                    fig.colorbar(im,cax=make_axes_locatable(ax[i,1]).append_axes("right","5%",pad="3%"),ticks=[np.min(self.components[i]),(np.max(self.components[i])-np.min(self.components[i]))/2.0,np.max(self.components[i])])
                 except:
                     pass
                                 
@@ -110,11 +139,11 @@ class MakeBSSPlots(object):
             for i in range(rows):
                 ax[i].set_ylabel(r'$I$ (arb. units)',fontsize=self.fs)
                 try:
-                    ax[i].plot(self.target[i],'.k',label='source')
+                    ax[i].plot(self.target[pairs[i][0]],'.k',label='source')
                 except:
                     pass
                 try:
-                    ax[i].plot(self.components[i][self.ts_cut,:],'r',label='prediction')
+                    ax[i].plot(self.components[pairs[i][1]][self.ts_cut,:],'r',label='prediction')
                 except:
                     pass
                 if i == rows-1:
@@ -132,10 +161,12 @@ class MakeBSSPlots(object):
 
 
     def plot_div(self,**kwargs):
+        """Plot divergence metric as function of iteration"""
         fig = plt.figure(figsize=self.fig_size)
         ax = fig.gca()
         ax.plot(self.div)
         ax.set_yscale('log')
+        ax.set_xlim([0,len(self.div)])
         ax.set_title(r'Divergence Measure',fontsize=self.fs)
         ax.set_xlabel(r'iteration',fontsize=self.fs)
         ax.set_ylabel(r'$d(T,A)$',fontsize=self.fs)
@@ -145,31 +176,29 @@ class MakeBSSPlots(object):
         else:
             plt.show()
             
-    def make_components(self):
-        """Rotate matrices back"""
-        
-        if self.angle != 0:
-            self.A  = self.crop_and_rotate(self.A,self.angle)
-            for i in range(self.q):
-                self.components.append(self.crop_and_rotate(np.outer(self.u[:,i],self.v[i,:]),self.angle))
-        else:
-            for i in range(self.q):
-                self.components.append(np.outer(self.u[:,i],self.v[i,:]))
+    def match_closest(self):
+        """Create list of pairs of components and targets so that the plots correspond"""
+        sources = range(self.q)
+        pairs = []
+        i_target = 0
+        while sources != []:
+            min_diff = 1.0e+50
+            pairs.append(([],[]))
             
-    def crop_and_rotate(self,x_mat,angle):
-    
-        #Find the backgound value
-        bg_val = np.min(x_mat[np.where(x_mat>np.max(x_mat)/100.0)])
-    
-        #Rotate the image and interpolate as necessary
-        x_rot = rotate(x_mat,angle)
-    
-        #Find bounds by subtracting out background
-        row_bounds,col_bounds = np.where(x_rot>bg_val)
-        top = np.min(row_bounds)
-        bottom = np.max(row_bounds)
-        left = np.min(col_bounds)
-        right = np.max(col_bounds)
-    
-        #Return trimmed matrix
-        return x_rot[top:bottom,left:right]
+            for i in sources:
+                if self.input_type == 'matrix':
+                    diff = np.mean(np.fabs(self.target[i_target] - self.components[i]))
+                else:
+                    diff = np.mean(np.fabs(self.target[i_target] - self.components[i][self.ts_cut,:]))
+                
+                if diff < min_diff:
+                    pairs.pop()
+                    pairs.append((i_target,i)) 
+                    min_diff = diff
+                    
+            sources.remove(pairs[i_target][1])
+            i_target += 1
+            
+        return pairs
+                
+                